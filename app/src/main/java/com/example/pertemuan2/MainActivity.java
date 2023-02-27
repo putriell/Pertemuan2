83212624590c35e1ad17969d8e888d3229eaae0e
asus <test@gmail.com>
2023-02-27 18:51:39 +0700

    dinamis

--- app/src/main/java/com/example/pertemuan2/MainActivity.java
+++ app/src/main/java/com/example/pertemuan2/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.pertemuan2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView isi;
+    private Button mButton;
+    private Boolean isFragmentDisplayed = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        isi = findViewById(R.id.fragment);
+        mButton = findViewById(R.id.button1);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isFragmentDisplayed){
+                    displayFragment();
+                } else {
+                   closeFragment();
+                }
+            }
+        });
+    }
+
+    public void displayFragment(){
+        SimpleFragment simpleFragment = SimpleFragment.newInstance();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.fragment_container,simpleFragment).commit();
+        isi.setText(R.string.dinamis);
+        mButton.setText("Mantap!");
+        isFragmentDisplayed = true;
+    }
+
+    public void closeFragment(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        SimpleFragment simpleFragment = (SimpleFragment) fragmentManager
+                .findFragmentById(R.id.fragment_container);
+
+        if (simpleFragment != null){
+            FragmentTransaction fragmentTransaction =
+                    fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, simpleFragment).commit();
+        }
+
+        isi.setText(R.string.isi);
+        mButton.setText("Lihat Kelebihan");
+        isFragmentDisplayed = false;
     }
 }
--- app/src/main/java/com/example/pertemuan2/SimpleFragment.java
+++ app/src/main/java/com/example/pertemuan2/SimpleFragment.java
@@ -13,8 +13,9 @@ public class SimpleFragment extends Fragment {
         // Required empty public constructor
     }
 
-
-
+    public static SimpleFragment newInstance(){
+        return new SimpleFragment();
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
